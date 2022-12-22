5fb724a5adf955fc523a3163badaf4f8530b9718
Harry211201 <116766787+Harry211201@users.noreply.github.com>
2022-12-21 23:13:55 -0500

    Subiendo servidor

---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+	name: {
+		type: String,
+		require: true,
+	},
+	email: {
+		type: String,
+		require: true,
+	},
+	user: {
+		type: String,
+		require: true,
+	},
+	password: {
+		type: String,
+		require: true,
+	},
+	favorites: {
+		type: Object,
+		properties: {
+			id: {
+				type: String,
+			},
+		},
+	},
+});
+
+module.exports = mongoose.model("User", userSchema);
